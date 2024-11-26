f304c380e19f44f65f95c52cd83d263d4d3ac76f
Froggit2 <rus.yakovlev.03@gmail.com>
2024-11-26 08:15:02 +0300

    fastapi

--- FastAPI.py
+++ FastAPI.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 import sqlite3
 
+app = FastAPI()
 templates = Jinja2Templates(directory='templates')
 
 connection = sqlite3.connect('FastAPI.db', check_same_thread=False)
@@ -17,34 +18,20 @@ password TEXT
 ''')
 connection.commit()
 
-app = FastAPI()
-
-
 @app.get('/')
 async def glavn_html(request: Request):
     return templates.TemplateResponse('glavn.html', {'request': request})
-
 
 @app.get('/registr')
 async def registrat_html(request: Request):
     return templates.TemplateResponse('registr.html', {'request': request})
 
-
-@app.get('/login')
-async def login_html(request: Request):
-    return templates.TemplateResponse('login.html', {'request': request})
-
-
-@app.get('/logout')
-async def logout_html(request: Request):
-    return templates.TemplateResponse('logout.html', {'request': request})
-
-
 @app.post('/register')
 async def registration(request: Request):
-    login = request.get('login')
-    password_1 = request.get('password_1')
-    password_2 = request.get('password_2')
+    form = await request.form()
+    login = form.get('login')
+    password_1 = form.get('password_1')
+    password_2 = form.get('password_2')
     if password_1 != password_2:
         error = 'Пароли должны совпадать!'
         return templates.TemplateResponse('registr.html', {
@@ -52,7 +39,7 @@ async def registration(request: Request):
             'error': error
         })
     cursor.execute('SELECT * FROM Users WHERE login = ?', (login,))
-    if login == cursor.fetchone():
+    if cursor.fetchone():
         error = 'Логин не должен совпадать с логином другого пользователя!'
         return templates.TemplateResponse('registr.html', {
             'request': request,
@@ -66,51 +53,38 @@ async def registration(request: Request):
         'message': 'Регистрация прошла успешно!'
     })
 
+@app.get('/login')
+async def login_html(request: Request):
+    return templates.TemplateResponse('login.html', {'request': request})
 
 @app.post('/login')
 async def login(request: Request):
-    login = request.get('login')
-    password = request.get('password')
-    cursor.execute('SELECT * FROM Users WHERE login = ?',
-                   (login,))
-    if login != cursor.fetchone():
-        error = 'Неправильный логин или пароль'
-        return templates.TemplateResponse('login.html', {
-            'request': request, 'error': error
-        })
-    cursor.execute('SELECT * FROM Users WHERE password = ?',
-                   (password,))
-    if password != cursor.fetchone():
+    form = await request.form()
+    login = form.get('login')
+    password = form.get('password')
+    cursor.execute('SELECT * FROM Users WHERE login = ? AND password = ?',
+                   (login, password))
+    if not cursor.fetchone():
         error = 'Неправильный логин или пароль'
         return templates.TemplateResponse('login.html', {
             'request': request, 'error': error
         })
     message = 'Вы вошли успешно!'
     return templates.TemplateResponse('login.html', {
-        'request': Request, 'message': message
+        'request': request, 'message': message
     })
 
+@app.get('/logout')
+async def logout_html(request: Request):
+    return templates.TemplateResponse('logout.html', {'request': request})
 
-@app.delete('/logout')
+@app.post('/logout')
 async def logout(request: Request):
-    login = request.get('login')
-    password = request.get('password')
-    cursor.execute('SELECT * FROM Users WHERE login = ?',
-                   (login,))
-    if login != cursor.fetchone():
-        error = 'Неправильный логин или пароль'
-        return templates.TemplateResponse('login.html', {
-            'request': request, 'error': error
-        })
-    cursor.execute('SELECT * FROM Users WHERE password = ?',
-                   (password,))
-    if password != cursor.fetchone():
-        error = 'Неправильный логин или пароль'
-        return templates.TemplateResponse('login.html', {
-            'request': request, 'error': error
-        })
+    login = request.form.get('login')
+    password = request.form.get('password')
+    cursor.execute('DELETE FROM Users WHERE login = ? AND password = ?',
+                   (login, password))
     message = 'Вы успешно вышли!'
-    return templates.TemplateResponse('login.html', {
-        'request': request,
-        'message': message
+    return templates.TemplateResponse('logout.html', {
+        'request': request, 'message': message
     })
